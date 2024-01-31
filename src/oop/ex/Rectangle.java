5f21cc85e850de31a50c72e7a00b0d4aae9e00aa
tmdals1207 <ghdtmdalsdad@naver.com>
2024-01-31 18:34:47 +0900

    (01/31) Oop

---
+++ src/oop/ex/Rectangle.java
@@ -0,0 +1,16 @@
+package oop.ex;
+
+public class Rectangle {
+    int width = 0;
+    int height = 0;
+
+    int calculateArea() {
+        return width * height;
+    }
+    int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+    boolean isSquare() {
+        return width == height;
+    }
+}
