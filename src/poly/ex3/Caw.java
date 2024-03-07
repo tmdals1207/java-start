a780399875e9db1939ac04aa642b98317bb16527
tmdals1207 <ghdtmdalsdad@naver.com>
2024-03-07 23:42:35 +0900

    (03/07) Abstract

---
+++ src/poly/ex3/Caw.java
@@ -0,0 +1,7 @@
+package poly.ex3;
+public class Caw extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("음매");
+    }
+}
---
+++ src/poly/ex4/AbstractAnimal.java
@@ -0,0 +1,5 @@
+package poly.ex4;
+public abstract class AbstractAnimal {
+    public abstract void sound();
+    public abstract void move();
+}
